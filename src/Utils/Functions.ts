3e778ca2a5174c12ed1c82b78e1a2b3cfe3fccaa
Mizab <mizabmansari@gmail.com>
2023-10-04 22:26:29 +0530

    Added excluded ranged function

--- src/Utils/Functions.ts
+++ src/Utils/Functions.ts
@@ -35,3 +35,37 @@ export function randomIntFromInterval(min: number, max: number): number {
 export function randomFloatFromInterval(min: number, max: number): number {
   return Math.random() * (max - min + 1) + min;
 }
+
+/**
+ * Generates a random number within a given range, excluding certain values.
+ *
+ * @param {number} min - The minimum value of the range.
+ * @param {number} max - The maximum value of the range.
+ * @param {number[]} exclude - An array of values to exclude from the range.
+ * @returns {number | null} - A random number within the range, excluding the specified values. Returns null if all values are excluded.
+ */
+export function getRandomNumberInRange(
+  min: number,
+  max: number,
+  exclude: number[]
+): number | null {
+  if (max <= min) {
+    throw new Error("Invalid range. 'max' should be greater than 'min'.");
+  }
+
+  const range = Array.from(
+    { length: max - min + 1 },
+    (_, index) => min + index
+  );
+
+  // Filter out excluded values
+  const availableValues = range.filter((value) => !exclude.includes(value));
+
+  if (availableValues.length === 0) {
+    // All values are excluded, return null or handle it as you like
+    return null;
+  }
+
+  const randomIndex = Math.floor(Math.random() * availableValues.length);
+  return availableValues[randomIndex];
+}
